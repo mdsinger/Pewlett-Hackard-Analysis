97ba08c19edd01320be8c835ab27d9feba8186d6
Melissa Singer <mdsinger15@gmail.com>
2023-01-02 15:27:59 -0700

    updating schema

--- Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/schema.sql
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/schema.sql
@@ -33,18 +33,17 @@ CREATE TABLE salaries(
 );
 CREATE TABLE dept_emp(
 	emp_no INT NOT NULL,
-	dept_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 	PRIMARY KEY (emp_no)
 );
+
 CREATE TABLE titles(
 	emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
-SELECT * FROM departments;
